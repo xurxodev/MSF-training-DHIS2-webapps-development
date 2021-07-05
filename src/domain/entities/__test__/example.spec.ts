e224de799fd75e2cb154374a37f003decd5d872a
Jorge Sánchez Fernández <xurxosf@gmail.com>
2021-07-05 16:12:24 +0200

    Add example test

---
+++ src/domain/entities/__test__/example.spec.ts
@@ -0,0 +1,14 @@
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+beforeEach(() => {});
+afterEach(() => {});
+
+describe("Example group1", () => {
+    describe("Example group2", () => {
+        it("adds 1 + 2 to equal 3", () => {
+            expect(sum(1, 2)).toBe(3);
+        });
+    });
+});
